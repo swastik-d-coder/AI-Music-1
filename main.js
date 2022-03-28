a711893b5a53e3871c5ae31756965135cced06cd
swastik-d-coder <102283282+swastik-d-coder@users.noreply.github.com>
2022-03-28 18:44:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song = "";
 song2 = "";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload() {
     song = loadSound("Wahran - Randall(PagalWorld) (1).mp3");
@@ -12,8 +16,29 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet Is Initialized');
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+}
+
+function gotPoses() {
+    if(results.lenght > 0) {
+console.log(results);
+
+leftWristX = resluts[0].pose.leftWrist.x;
+leftWristY = resluts[0].pose.leftWrist.y;
+console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+rightWristX = resluts[0].pose.rightWrist.x;
+rightWristY = resluts[0].pose.rightWrist.y;
+console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
